f17cdb5ac9feefd4d5a1560bcda017715b7053c3
世纪糕 <75254963+con123gao@users.noreply.github.com>
2023-02-13 17:53:08 +0800

    fix: a

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// REM
+import 'lib-flexible';
+// 处理最大宽度
+(function(){
+    const handleMax = function handleMax(){
+        let html = document.documentElement,
+        root=document.getElementById('root'),
+        // size=parseFloat(html.style.fontSize);
+            deviceW=html.clientWidth;
+        root.style.maxWidth = "750px";
+        if(deviceW>=750){
+            html.style.fontSize='75px';
+        }
+    };
+    handleMax();
+    window.addEventListener('resize',handleMax);
+})();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
